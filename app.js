8cb6b9bdb90e25e0a43d8ce7592cf1da370aad3b
Matamala Daniel <daniel20mat@gmail.com>
2023-01-12 08:13:16 -0300

    modularizando

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-
-const dotenv = require("dotenv")
-dotenv.config()
 const express = require('express')
 const app = express()
+const config = require("./utils/config")
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./routes/blogs')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', true);
+logger.info('connecting to', config.MONGODB_URI)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl).then( console.log("BD connected"))
+mongoose.set('strictQuery', true)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blog', blogRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
