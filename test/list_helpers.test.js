07400333d48630aade1acf94be9afd4b41d9b3d5
Matamala Daniel <daniel20mat@gmail.com>
2023-01-14 07:24:22 -0300

    agregando testing

---
+++ test/list_helpers.test.js
@@ -0,0 +1,36 @@
+const listHelper = require('../utils/list_helper')
+const { blogs } = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+})
+
+describe('favorite blog', () => {
+  const listBlogs = []
+
+  test('select blog with most likes', () => {
+    const result = listHelper.maxLikes(listBlogs)
+    expect(result).toEqual(null)
+  })
+})
